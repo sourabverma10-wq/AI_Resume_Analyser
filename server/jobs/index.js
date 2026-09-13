8a7a1f3d88ff996743b93de0bb8414ecf8dfe7fc
sourabverma10-wq <sourabverma10@gmail.com>
2026-09-13 18:34:33 +0530

    Fix bundled job definitions

--- server/jobs/index.js
+++ server/jobs/index.js
@@ -1,7 +1,8 @@
 import softwareEngineer from './software-engineer.js';
 import frontendDeveloper from './frontend-developer.js';
 
-export const jobs = [softwareEngineer, frontendDeveloper];
+const unwrapJob = jobModule => jobModule?.default || jobModule;
+export const jobs = [unwrapJob(softwareEngineer), unwrapJob(frontendDeveloper)];
 
 export function findJob(id) {
   return jobs.find(job => job.id === id);
